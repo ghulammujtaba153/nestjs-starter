c4148411b3f74a6f2395ff396cc675c408160cee
ghulammujtaba153 <ghulam.mujtaba1016@gmail.com>
2025-01-22 15:45:25 +0500

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  // getHello(id: string, role: string): string {
+  //   return `Hello ${role}! Your ID is ${id}.`;
+  // }
+}
